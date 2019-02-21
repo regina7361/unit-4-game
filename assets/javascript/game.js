5adf7035556880c10efc4b2046a946cd21861398
Regina Garcia <regina7361@gmail.com>
2019-02-21 14:56:24 -0800

    updated click logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,7 @@
+var totalScore = 0;
+var wins = 0;
+var losses = 0;
+
 //select a random number
 var randomNumber = Math.floor(Math.random() * 100) + 1;
 console.log(randomNumber);
@@ -5,19 +9,27 @@ console.log(randomNumber);
 //insert random number into the Random Number box on page load
 function setup() {
     document.getElementById('randomNum').innerHTML = randomNumber;
-    $(".crystals").click(handleClick);
+    
+//on click assign a random number for each crystal
+    console.log("testing")
+    for(let i = 0; i < 4; i++) {
+    let crystalNum = Math.floor(Math.random() * 10) + 1;
+    $("[data-id=cry" + i + "]").attr("value", crystalNum);
+    console.log($("[data-id=cry" + i + "]"))
+}
+
+$(".crystals").click(handleClick);
 }
 
 //make crystals clickable
 function handleClick(e) {
     var x = e.currentTarget.innerText;
-
-//on click assign a random number for each crystal
-    var crystalNum = Math.floor(Math.random() * 10) + 1;
-    x = crystalNum;
+    let clickedNum = parseInt($(this).attr('value'));
+    console.log(clickedNum);
+    totalScore += clickedNum;
 
 //when a user clicks a crystal display number in Total Score box
-    document.getElementById('totalScore').innerHTML = crystalNum;
+    $("#totalScore").text(totalScore);
     console.log(x);
 }
 
